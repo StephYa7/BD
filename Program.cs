1628eff9f047f2f797494b6345c93a0d87e5b347
Steph <1000p@mail.ru>
2022-04-01 07:26:53 +0700

    add newLenght

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+
+string[] startString = new string[] {"hell22o", "tt", "123", "1234", "jaja0", "s"};
+int newLenght = 0;
+
+
+for (int i = 0; i < startString.Length; i++)
+{
+    if (startString[i].Length <= 3)
+    {
+        newLenght += 1;
+    }
+
+}
+
+
+
+WriteLine(string.Join(",", startString));
+WriteLine(startString[0].Length);
+WriteLine(newLenght);
